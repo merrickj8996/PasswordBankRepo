68a44a05d6d948074a15cda99bb08cc46346b079
ulesichd <71786204+ulesichd@users.noreply.github.com>
2021-02-14 12:45:25 -0600

    Added checkStrength method with comments.

--- PasswordBank/WindowsFormsApp1/Program.cs
+++ PasswordBank/WindowsFormsApp1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
 
 namespace WindowsFormsApp1
 {
@@ -22,6 +23,102 @@ namespace WindowsFormsApp1
 
     static class Password
     {
+        static double checkStrength(string password) {
+            // Initalizes double to store password strength at sets it to 0.
+            double passwordStrength = 0;
 
+            // If the password isn't more than 4 characters the password strength will be 0.
+            if (password.Length <= 4) {
+                // Changing text color to red.
+                Console.ForegroundColor = ConsoleColor.Red;
+                return passwordStrength;
+            }
+
+            else {
+
+                // If the password length is more than 4 it gets a score of at least 1 before multiplier.
+                passwordStrength = 1;
+
+                // If the password length is at least 8 it gets a score of at least 2 before multiplier.
+                if (password.Length >= 8) {
+                    passwordStrength = 2;
+                }
+
+                // If the password length is at least 12 it gets a score of at least 3 before multiplier.
+                if (password.Length >= 12) {
+                    passwordStrength = 3;
+                }
+
+                // If the password length is at tleast 15 it gets a score of at least 4 before multiplier.
+                if (password.Length >= 15) {
+                    passwordStrength = 4;
+                }
+
+                // If the password contains a space it gets an increase of 1.
+                if (Regex.IsMatch(password, @" ")) {
+                    passwordStrength++;
+                }
+
+                // If the password contains a number it gets an increase of 1.
+                if (Regex.IsMatch(password, @"\d")) {
+                    passwordStrength++;
+                }
+
+                // If the password contains a upper and lower case letter it gets an increase of 1.
+                if (Regex.IsMatch(password, @"[a-z]") && Regex.IsMatch(password, @"[A-Z]")) {
+                    passwordStrength++;
+                }
+
+                // If the password contains a special character it gets an increase of 1.
+                if (Regex.IsMatch(password, @"[!,@,#,$,%,^,&,*,?,_,~,£,(,)]")) {
+                    passwordStrength++;
+                }
+
+                // Sets the first character in the password to temp
+                char temp = password[0];
+                // Every password will have at least one special character
+                int uniqueCharacters = 1;
+                for (int i = 0; i < password.Length; i++) {
+                    // If the current character is unique then it becomes the new temp 
+                    // and uniqueCharacters increaes by 1
+                    if (password[i] != temp) {
+                        temp = password[i];
+                        uniqueCharacters++;
+                    }
+                }
+
+                // multiplier is intialized and set to 0.
+                double mulitplier = 0;
+
+                // If there are less than 2 unique characters the password strength is 0.
+                if (uniqueCharacters >= 2) {
+                    mulitplier = .5;
+                }
+
+                // If there are less than 5 unique characters, but at least 2 then the password strength
+                // will be cut in half.
+                if (uniqueCharacters >= 5) {
+                    mulitplier = 1;
+                }
+
+                // If there are less than 8 unique characters, but at least 5 then the password strength
+                // will be left the same.
+                if (uniqueCharacters >= 8) {
+                    mulitplier = 1.5;
+                }
+
+                // If there are less than 10 unique characters, but at least 8 then the password strength
+                // will be increased by 50%.
+                // If there are at least 10 unique characters the password strength is doubled.
+                if (uniqueCharacters >= 10) {
+                    mulitplier = 2;
+                }
+
+                // Multiplier is applied to password strength.
+                passwordStrength = passwordStrength * mulitplier;
+
+                return passwordStrength;
+            }
+        }
     }
 }
