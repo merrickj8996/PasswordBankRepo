b02ae91916e58c5608b0ac416491e7193dd49e30
Brendan <bvoigt00@gmail.com>
2021-02-15 12:39:44 -0600

    Added compress unit test

--- PasswordBank/PasswordBankTests/PasswordBankTests.cs
+++ PasswordBank/PasswordBankTests/PasswordBankTests.cs
@@ -1,9 +1,26 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using WindowsFormsApp1;
+using System.IO;
 
 namespace PasswordBankTests {
     [TestClass]
     public class PasswordBankTests {
-        
+
+        [TestMethod]
+        public void compressTest() {
+            // Arrange
+            string toCompressPath = @"..\..\..\TestResources\CompressTestResources";
+            string compressedPath = toCompressPath + @"\Test.txt.gz";
+            DirectoryInfo toCompressDirectory = new DirectoryInfo(toCompressPath);
+
+            // Act
+            BankFile.Compress(toCompressDirectory);
+
+            // Assert
+            Assert.IsNotNull(compressedPath);
+
+            // Clean-up
+            File.Delete(compressedPath);
+        }
     }
 }
