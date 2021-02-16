685dbbd47c02564e70f3d2066495d6abba270940
Jackson <Jackson@POTATOPURIFIER>
2021-02-16 17:20:54 -0600

    Adding Crypto class for file encryption

--- PasswordBank/WindowsFormsApp1/Program.cs
+++ PasswordBank/WindowsFormsApp1/Program.cs
@@ -176,4 +176,96 @@ namespace WindowsFormsApp1
             }
         }
     }
+    static class Crypto {
+        #region memberVariables
+        private static int mIter = 50000;
+        private static int mKeyLength = 256;
+        private static int mBlockSize = 128;
+        #endregion
+
+        //Generates Salt for use with password. 
+        public static byte[] SaltGen() {
+            byte[] salt = new byte[32];
+            using (RNGCryptoServiceProvider rngCryptoServiceProvider = new RNGCryptoServiceProvider()) {
+                rngCryptoServiceProvider.GetBytes(salt);
+            }
+            return salt;
+        }
+
+        //Takes a file name and password and encrypts the file. 
+        public static void EncryptFile(string inFile, string password) {
+            byte[] salt = SaltGen();
+            byte[] passwords = Encoding.UTF8.GetBytes(password);
+            AesManaged AES = new AesManaged();
+            AES.KeySize = mKeyLength;
+            AES.BlockSize = mBlockSize;
+            AES.Padding = PaddingMode.PKCS7;
+            var key = new Rfc2898DeriveBytes(passwords, salt, mIter);
+            AES.Key = key.GetBytes(AES.KeySize / 8);
+            AES.IV = key.GetBytes(AES.BlockSize / 8);
+            //AES.Mode = CipherMode.CFB;
+            try {
+                using (FileStream fileCrypto = new FileStream(inFile + ".aes", FileMode.Create)) {
+                    fileCrypto.Write(salt, 0, salt.Length);
+                    using (CryptoStream cryptoStream = new CryptoStream(fileCrypto, AES.CreateEncryptor(), CryptoStreamMode.Write)) {
+                        using (FileStream fileStreamIn = new FileStream(inFile, FileMode.Open)) {
+                            byte[] buffer = new byte[1048576];
+                            int read;
+                            while ((read = fileStreamIn.Read(buffer, 0, buffer.Length)) > 0) {
+                                cryptoStream.Write(buffer, 0, read);
+                            }
+                        }
+                    }
+                    File.Delete(inFile);
+                }
+            }
+            catch (CryptographicException ex_CryptographicException) {
+                Console.WriteLine("CryptograpicException error: " + ex_CryptographicException);
+                File.Delete(inFile + ".aes");
+            }
+            catch (Exception ex) {
+                Console.WriteLine("Error: " + ex.Message);
+                File.Delete(inFile + ".aes");
+            }
+
+        }
+        public static void DecryptFile(string inFile, string outFile, string password) {
+            byte[] passwords = Encoding.UTF8.GetBytes(password);
+            byte[] salt = new byte[32];
+            using (FileStream fileCrypto = new FileStream(inFile, FileMode.Open)) {
+                fileCrypto.Read(salt, 0, salt.Length);
+                AesManaged AES = new AesManaged();
+                AES.KeySize = mKeyLength;
+                AES.BlockSize = mBlockSize;
+                var key = new Rfc2898DeriveBytes(passwords, salt, mIter);
+                AES.Key = key.GetBytes(AES.KeySize / 8);
+                AES.IV = key.GetBytes(AES.BlockSize / 8);
+                AES.Padding = PaddingMode.PKCS7;
+                //AES.Mode = CipherMode.CFB;
+                try {
+                    using (CryptoStream cryptoStream = new CryptoStream(fileCrypto, AES.CreateDecryptor(), CryptoStreamMode.Read)) {
+                        using (FileStream fileStreamOut = new FileStream(outFile, FileMode.Create)) {
+                            int read;
+                            byte[] buffer = new byte[1048576];
+                            while ((read = cryptoStream.Read(buffer, 0, buffer.Length)) > 0) {
+                                fileStreamOut.Write(buffer, 0, read);
+                            }
+                        }
+                    }
+                    File.Delete(inFile);
+                }
+                catch (CryptographicException ex_CryptographicException) {
+                    Console.WriteLine("CryptograpicException error: " + ex_CryptographicException);
+                    File.Delete(outFile);
+                }
+                catch (Exception ex) {
+                    Console.WriteLine("Error: " + ex.Message);
+                    File.Delete(outFile);
+                }
+
+            }
+
+        }
+    }
 }
+
