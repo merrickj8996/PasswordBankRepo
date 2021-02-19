177b206ea99ebce5e16ff202eecf79c79acc8aa4
Ian Klein <kleini2521@my.uwstout.edu>
2021-02-19 14:42:43 -0600

    Added Randomizer Functionality to Password Randomizer for Testing

--- PasswordBank/WindowsFormsApp1/PasswordRandomizer.cs
+++ PasswordBank/WindowsFormsApp1/PasswordRandomizer.cs
@@ -1,10 +1,13 @@
 ﻿namespace WindowsFormsApp1 {
+    using System;
+    using System.Security.Cryptography;
     static partial class Password {
-        public static string randomizePassword(int length, bool includeUppercase, bool includeNumbers, bool includeSymbols) {
+        public static string randomizePassword(int length, bool includeUppercase, bool includeNumbers, bool includeSymbols, bool includeBrackets) {
             string chars = "abcdefghijklmnopqrstuvwxyz";
             string uppers = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
             string numbers = "1234567890";
-            string symbols = "~`!@#$%^&*_-+=,.?/";
+            string symbols = "~`!@#$%^&*_-+'=,.;:?/";
+            string brackets = "(){}[]";
 
             string genPass = "";
 
@@ -20,8 +23,18 @@
                 chars.Insert(chars.Length, symbols);
             }
 
+            if (includeBrackets) {
+                chars.Insert(chars.Length, brackets);
+            }
+
             for (int i = 0; i > length; i++) {
-                genPass.Insert(genPass.Length, /*random thing here*/ "a");
+                byte[] data = new byte[4];
+                RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+                rng.GetBytes(data);
+
+                int value = BitConverter.ToInt32(data, 0) % chars.Length;
+
+                genPass.Insert(genPass.Length, chars[value].ToString());
             }
 
             return genPass;
