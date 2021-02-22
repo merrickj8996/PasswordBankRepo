1b4186e6930e75dad1a453ea269d0048355f606d
Brendan <bvoigt00@gmail.com>
2021-02-22 10:19:05 -0600

    Added print option for Master Password

---
+++ PasswordBank/WindowsFormsApp1/MasterPasswordPrintPopUp.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace WindowsFormsApp1 {
+    partial class MasterPasswordPrintPopUp {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.printButton = new System.Windows.Forms.Button();
+            this.masterPasswordPrintDialog = new System.Windows.Forms.PrintDialog();
+            this.printText = new System.Windows.Forms.Label();
+            this.noPrintButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // printButton
+            // 
+            this.printButton.Location = new System.Drawing.Point(60, 53);
+            this.printButton.Name = "printButton";
+            this.printButton.Size = new System.Drawing.Size(75, 23);
+            this.printButton.TabIndex = 0;
+            this.printButton.Text = "Yes";
+            this.printButton.UseVisualStyleBackColor = true;
+            this.printButton.Click += new System.EventHandler(this.printButton_Click);
+            // 
+            // masterPasswordPrintDialog
+            // 
+            this.masterPasswordPrintDialog.AllowSomePages = true;
+            this.masterPasswordPrintDialog.UseEXDialog = true;
+            // 
+            // printText
+            // 
+            this.printText.AutoSize = true;
+            this.printText.Location = new System.Drawing.Point(12, 9);
+            this.printText.Name = "printText";
+            this.printText.Size = new System.Drawing.Size(318, 13);
+            this.printText.TabIndex = 1;
+            this.printText.Text = "Would you like to print out a document containing your password?\r\n";
+            // 
+            // noPrintButton
+            // 
+            this.noPrintButton.Location = new System.Drawing.Point(194, 53);
+            this.noPrintButton.Name = "noPrintButton";
+            this.noPrintButton.Size = new System.Drawing.Size(75, 23);
+            this.noPrintButton.TabIndex = 2;
+            this.noPrintButton.Text = "No";
+            this.noPrintButton.UseVisualStyleBackColor = true;
+            this.noPrintButton.Click += new System.EventHandler(this.noPrintButton_Click);
+            // 
+            // MasterPasswordPrintPopUp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(340, 111);
+            this.Controls.Add(this.noPrintButton);
+            this.Controls.Add(this.printText);
+            this.Controls.Add(this.printButton);
+            this.Name = "MasterPasswordPrintPopUp";
+            this.Text = "MasterPasswordPrintPopUp";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button printButton;
+        private System.Windows.Forms.PrintDialog masterPasswordPrintDialog;
+        private System.Windows.Forms.Label printText;
+        private System.Windows.Forms.Button noPrintButton;
+    }
+}
---
+++ PasswordBank/WindowsFormsApp1/MasterPasswordPrintPopUp.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1 {
+    public partial class MasterPasswordPrintPopUp : Form {
+
+
+        private string m_masterPassword;
+
+        public MasterPasswordPrintPopUp() {
+            InitializeComponent();
+        }
+
+        public MasterPasswordPrintPopUp(string masterPassword) {
+            InitializeComponent();
+            m_masterPassword = masterPassword;
+        }
+
+        private PrintDocument masterPasswordDoc = new PrintDocument();
+
+        private void printButton_Click(object sender, EventArgs e) {
+
+            using (StreamWriter passwordFile = new StreamWriter("password.txt")) {
+                passwordFile.WriteLine("Password Bank Password: " + m_masterPassword);
+            }
+
+            using (StreamReader streamToPrint = new StreamReader("password.txt")) {
+                Font printFont = new Font("Arial", 10);
+                masterPasswordDoc.PrintPage += new PrintPageEventHandler(this.MasterPasswordDoc_PrintPage);
+            }
+
+            masterPasswordPrintDialog.Document = masterPasswordDoc;
+
+            DialogResult result = masterPasswordPrintDialog.ShowDialog();
+
+            if (result == DialogResult.OK)
+                masterPasswordDoc.Print();
+
+            File.Delete("password.txt");
+
+            this.Close();
+        }
+
+        // The PrintPage event is raised for each page to be printed.
+        private void MasterPasswordDoc_PrintPage(object sender, PrintPageEventArgs ev) {
+            float linesPerPage = 0;
+            float yPos = 0;
+            int count = 0;
+            float leftMargin = ev.MarginBounds.Left;
+            float topMargin = ev.MarginBounds.Top;
+            string line = null;
+            Font printFont = new Font("Arial", 10);
+
+            // Calculate the number of lines per page.
+            linesPerPage = ev.MarginBounds.Height / printFont.GetHeight(ev.Graphics);
+
+            using(StreamReader streamToPrint = new StreamReader("password.txt")) {
+                // Print each line of the file.
+                while (count < linesPerPage && ((line = streamToPrint.ReadLine()) != null)) {
+                    yPos = topMargin + (count * printFont.GetHeight(ev.Graphics));
+                    ev.Graphics.DrawString(line, printFont, Brushes.Black, leftMargin, yPos, new StringFormat());
+                    count++;
+                }
+            }
+
+            // If more lines exist, print another page.
+            if (line != null)
+                ev.HasMorePages = true;
+            else
+                ev.HasMorePages = false;
+        }
+
+        private void noPrintButton_Click(object sender, EventArgs e) {
+            this.Close();
+        }
+    }
+}
