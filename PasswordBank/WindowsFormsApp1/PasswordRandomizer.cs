bb815f3af7ddaae0a0d0d1ffcd7fe846effd29f2
Ian Klein <kleini2521@my.uwstout.edu>
2021-03-04 11:49:06 -0600

    added comments to password randomizaer

--- PasswordBank/WindowsFormsApp1/PasswordRandomizer.cs
+++ PasswordBank/WindowsFormsApp1/PasswordRandomizer.cs
@@ -3,38 +3,38 @@
     using System.Security.Cryptography;
     static partial class Password {
         public static string randomizePassword(int length, bool includeUppercase, bool includeNumbers, bool includeSymbols, bool includeBrackets) {
-            string chars = "abcdefghijklmnopqrstuvwxyz";
-            string uppers = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            string numbers = "1234567890";
-            string symbols = "~`!@#$%^&*_-+'=,.;:?/";
-            string brackets = "(){}[]";
+            string chars = "abcdefghijklmnopqrstuvwxyz"; //series of lowercase letters
+            string uppers = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; //series of uppercase letters
+            string numbers = "1234567890"; //series of numbers
+            string symbols = "~`!@#$%^&*_-+'=.;:?/"; //series of symbols
+            string brackets = "(){}[]"; //series of brackets
 
-            string genPass = "";
+            string genPass = ""; //blank string to be used as the final generated password
 
-            if (includeUppercase) {
+            if (includeUppercase) { //if passed in that the consumer wants uppercase letters, append "uppercase" to the chars string 
                 chars += uppers;
             }
 
-            if (includeNumbers) {
+            if (includeNumbers) { //if passed in that the consumer wants numbers, append "uppercase" to the chars string 
                 chars += numbers;
             }
 
-            if (includeSymbols) {
+            if (includeSymbols) { //if passed in that the consumer wants symbols, append "uppercase" to the chars string 
                 chars += symbols;
             }
 
-            if (includeBrackets) {
+            if (includeBrackets) { //if passed in that the consumer wants brackets, append "uppercase" to the chars string 
                 chars += brackets;
             }
 
-            for (int i = 0; i < length; i++) {
-                byte[] data = new byte[4];
-                RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
-                rng.GetBytes(data);
+            for (int i = 0; i < length; i++) { //loops grabbing a random character and appending to genPass a number of times equal to the requested length
+                byte[] data = new byte[4]; //creates a temporary byte for the rng variable to use in the randomization process
+                RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider(); //creates temporary rng variable
+                rng.GetBytes(data); //connects the rng variable to the data variable
 
-                int value = Math.Abs(BitConverter.ToInt32(data, 0) % chars.Length);
+                int value = Math.Abs(BitConverter.ToInt32(data, 0) % chars.Length); //take random number generated (mod the length of the chars string) to append
 
-                genPass += chars[value];
+                genPass += chars[value]; //add character at the index of the random number to the genPass variable
             }
 
             return genPass;
