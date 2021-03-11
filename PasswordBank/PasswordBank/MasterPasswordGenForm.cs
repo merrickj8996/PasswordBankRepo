66ecb4dd5b4a4cf5cc8f41836256c8c794b056fa
ulesichd <71786204+ulesichd@users.noreply.github.com>
2021-03-11 16:03:14 -0600

    Linked GridView1 with EntryVariablesPane so the data can be pulled and edited.

--- PasswordBank/PasswordBank/MasterPasswordGenForm.cs
+++ PasswordBank/PasswordBank/MasterPasswordGenForm.cs
@@ -19,13 +19,13 @@ namespace FirstPass {
         private void CloseButton_Click(object sender, EventArgs e) {
 
             int count = Convert.ToInt32(Math.Round(passSize.Value, 0));
-<<<<<<< HEAD
+
             //string pass = Password.randomizePassword(count, UpperCase.Checked, Digits.Checked, Special.Checked, Brackets.Checked);
             //Console.WriteLine(pass);
-=======
+
             String pass = Password.randomizePassword(count, UpperCase.Checked, Digits.Checked, Special.Checked, Brackets.Checked);
             Console.WriteLine(pass);
->>>>>>> 119d398b39bab0aa7417e35c6a3c9d9344241cb1
+
             PasswordOptions frm = new PasswordOptions();
             frm.Show();
             //frm.UpdateTextBox(pass);
