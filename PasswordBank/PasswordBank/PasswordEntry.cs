bb9330cab3c12e7cc83b4b1a86c9eb5f01e88250
Brendan <bvoigt00@gmail.com>
2021-03-11 10:00:09 -0600

    Implemented WriteToFile method

---
+++ PasswordBank/PasswordBank/PasswordEntry.cs
@@ -0,0 +1,32 @@
+﻿using CsvHelper.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstPass {
+
+    /// <summary>
+    /// Contains all the data for a password entry.
+    /// </summary>
+    public class PasswordEntry {
+        public int Id { get; set; }
+        public string Group { get; set; }
+        public string Title { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Url { get; set; }
+        public string Notes { get; set; }
+
+        public PasswordEntry(int id, string group, string title, string username, string password, string url, string notes) {
+            Id = id;
+            Group = group;
+            Title = title;
+            Username = username;
+            Password = password;
+            Url = url;
+            Notes = notes;
+        }
+    }
+}
