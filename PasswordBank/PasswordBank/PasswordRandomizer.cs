368de866e22c2bab2cf24f24749d9d5a6518062d
Ian Klein <kleini2521@my.uwstout.edu>
2021-03-11 14:50:26 -0600

    Added SecureString functionality to password randomizer and added function to compare SecureStrings

--- PasswordBank/PasswordBank/PasswordRandomizer.cs
+++ PasswordBank/PasswordBank/PasswordRandomizer.cs
@@ -1,15 +1,16 @@
 ﻿namespace FirstPass {
     using System;
     using System.Security.Cryptography;
+    using System.Security;
     static partial class Password {
-        public static string randomizePassword(int length, bool includeUppercase, bool includeNumbers, bool includeSymbols, bool includeBrackets) {
+        public static SecureString randomizePassword(int length, bool includeUppercase, bool includeNumbers, bool includeSymbols, bool includeBrackets) {
             string chars = "abcdefghijklmnopqrstuvwxyz"; //series of lowercase letters
             string uppers = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; //series of uppercase letters
             string numbers = "1234567890"; //series of numbers
             string symbols = "~`!@#$%^&*_-+'=.;:?/"; //series of symbols
             string brackets = "(){}[]"; //series of brackets
 
-            string genPass = ""; //blank string to be used as the final generated password
+            SecureString genPass = new SecureString(); //blank string to be used as the final generated password
 
             if (includeUppercase) { //if passed in that the consumer wants uppercase letters, append "uppercase" to the chars string 
                 chars += uppers;
@@ -34,7 +35,7 @@
 
                 int value = Math.Abs(BitConverter.ToInt32(data, 0) % chars.Length); //take random number generated (mod the length of the chars string) to append
 
-                genPass += chars[value]; //add character at the index of the random number to the genPass variable
+                genPass.AppendChar(chars[value]); //add character at the index of the random number to the genPass variable
             }
 
             return genPass;
